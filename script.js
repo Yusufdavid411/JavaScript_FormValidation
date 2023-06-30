f6cd380d77ac99a1e4634b358b41c0c5821e9241
yusuf david <yusufdavid@gmail.com>
2023-06-30 06:02:07 +0100

    form_validation

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("welcome")
+const password = document.getElementById("password")
+const message = document.getElementsByTagName("b")
+
+
+const paswd = password.value
+
+if (paswd.length >= 8){
+    message
+}
+
+
+
+
+
+
