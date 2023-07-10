79918e7b1d171d87d13b071824b9e62016e43dd6
yusuf david <yusufdavid@gmail.com>
2023-07-10 04:38:37 +0100

    max width of 300px

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 let message = document.getElementById("output");
 let password = document.getElementById('password');
+let email = document.getElementById('email');
 
 
 function myFunction() {
   const pass = password.value;
+  const eml = email.value;
+
   message.style.fontSize='1rem';
   message.style.color='red';
 
   if(pass.length == ""){
-    message.innerHTML='insert password';
+    message.innerHTML='insert password/email';
   }
-  else if(pass.length >= 6 ){
+  else if(pass.length >= 6 && eml.includes('@')){
     message.innerHTML='Successfully Submitted';
     message.style.color='green';
   }else{
-    message.innerHTML= 'password must be 8 or more character';
+    message.innerHTML= 'password must be 8 or more character and email must include @';
   }
   
 }
